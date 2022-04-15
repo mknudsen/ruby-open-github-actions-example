690e8e3052f2bdbca2cbe0ddca2cb9cd6e4e2383
Martin Knudsen <martin.knudsen@otto.de>
2022-04-15 09:44:30 +0200

    install ruby build deps and safe_yaml since the documentation sais so

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -xueo pipefail
+
+cd /mnt
+
+apk add build-base
+
 ruby -v
 
 gem install jekyll-import
+gem install safe_yaml
 
 ruby -w -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::RSS.run({
